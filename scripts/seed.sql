4e0c8e412a963ef9936c09de0268f090507a5973
Semyonov Denis <justdanru@gmail.com>
2023-05-04 23:36:52 +0300

    Мелкие косметические изменения

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -1,2 +1,1 @@
-INSERT INTO users (name, surname, last_name, login, password, salt, role, tg_id, created_at, updated_at, deleted_at) VALUES
-("Иван", "Петров", "Сергеевич", "admin01", "fc93e7998b2d184c101006e800c8a9c6fe74b5d99a428d0364ad41eaed79eece", "@vO5Yb538i(lTESZS%Rs3oq88C)A,hrU", 0, NULL, "2023-03-01 12:00:00", NULL, NULL);
+INSERT INTO users (name, surname, last_name, login, password, salt, role, tg_id, created_at, updated_at, deleted_at) VALUES ("Иван", "Петров", "Сергеевич", "admin01", "fc93e7998b2d184c101006e800c8a9c6fe74b5d99a428d0364ad41eaed79eece", "@vO5Yb538i(lTESZS%Rs3oq88C)A,hrU", 0, NULL, "2023-03-01 12:00:00", NULL, NULL);
