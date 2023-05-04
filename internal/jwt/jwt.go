7e71b893f10b2af1db41c8342caa35891d7ac2ca
Semyonov Denis <justdanru@gmail.com>
2023-05-04 23:35:53 +0300

    Добавил пакет jwt с базовым функционалом

---
+++ internal/jwt/jwt.go
@@ -0,0 +1,48 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"graphlabsts.core/internal/models"
+)
+
+var jwtSecretKey = []byte("secret JWT key")
+
+const (
+	AUTH_TOKEN_DURATION_MINUTES  = 5
+	REFRESH_TOKEN_DURATION_HOURS = 5
+)
+
+func createToken(userID int64, userRoleCode int64, expTime time.Time) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":   userID,
+		"role": userRoleCode,
+		"exp":  expTime,
+	})
+
+	tokenString, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func CreateAuthToken(uad *models.UserAuthData) (string, error) {
+	tokenString, err := createToken(uad.Id, uad.RoleCode, time.Now().Add(AUTH_TOKEN_DURATION_MINUTES*time.Minute))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func CreateRefreshToken(uad *models.UserAuthData) (string, error) {
+	tokenString, err := createToken(uad.Id, uad.RoleCode, time.Now().Add(REFRESH_TOKEN_DURATION_HOURS*time.Hour))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
